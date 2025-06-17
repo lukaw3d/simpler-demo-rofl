df65d95f35809aaaef377ed81b6c58c28eb27dca
lukaw3d <luka@oasisprotocol.org>
2025-06-18 01:46:29 +0200

    Simplify

--- README.md
+++ README.md
@@ -1,33 +1,17 @@
 # Demo ROFL App
 
 This is a demo ROFL app that runs in TDX using Podman containers defined in a
-`compose.yaml` file. It implements a simple price oracle in a shell script!
+`compose.yaml` file.
+
+docker/app.sh posts a message to 0x5d683b980615A7A60B3cFf3DFC338A9985278fF3 when
+ROFL app runs.
+
+https://explorer.oasis.io/testnet/sapphire/address/0x5d683b980615A7A60B3cFf3DFC338A9985278fF3
+is Echo.sol - it allows anyone to post a message.
 
 ## Documentation
 
 See [the ROFL documentation] for more details about ROFL-specific features
-available to ROFL apps and on how to deploy the apps. See [the tutorial] on how
-to create your own version of this app.
+available to ROFL apps and on how to deploy the apps.
 
 [the ROFL documentation]: https://docs.oasis.io/build/rofl
-[the tutorial]: https://docs.oasis.io/build/rofl/app
-
-## Oracle Smart Contract
-
-The `/oracle` directory contains the smart contract code for the oracle implementation, sourced from the [Oasis SDK rofl-oracle example](https://github.com/oasisprotocol/oasis-sdk/tree/main/examples/runtime-sdk/rofl-oracle/oracle). It includes:
-
-- Solidity smart contract (`contracts/Oracle.sol`)
-- Hardhat configuration and deployment scripts
-
-To update this contract code with the latest version:
-
-```bash
-# Run from the root directory of this repository
-./scripts/update-oracle.sh
-```
-
-## Existing Deployment
-
-The `rofl.yaml` in this repository defines an existing deployment named `test`
-that is used for testing of the demo ROFL app. If you want to modify any of the
-application aspects, you need to create your own deployment.
--- docker/app.sh
+++ docker/app.sh
@@ -1,24 +1,17 @@
 #!/bin/sh
 
-while true; do
-	# Fetch a recent price from Binance.
-	price=$(curl -s "https://www.binance.com/api/v3/ticker/price?symbol=${TICKER}" | jq '(.price | tonumber) * 1000000 | trunc')
-	if [ -z "$price" ]; then
-		sleep 15
-		continue
-	fi
+# Format calldata
+MESSAGE="ab dfg dfkogdfk ogk ortz ortzk otzki54 6ki54o0 6540o6 o05kg"
+# emitMessage("ab") should encode as 0x2ac0df26000000000000000000000000000000000000000000000000000000000000002000000000000000000000000000000000000000000000000000000000000000026162000000000000000000000000000000000000000000000000000000000000
+MESSAGE="${MESSAGE:0:32}" # this bash abi encoding doesn't work correctly for long strings
+method="2ac0df26" # keccak256("emitMessage(string)")
+stringtype="0000000000000000000000000000000000000000000000000000000000000020"
+stringlen=$(printf "%064x" "$(echo -n "${MESSAGE}" | wc -c)")
+stringval=$(printf "%s" "$(printf "%-64s" "$(echo -n "${MESSAGE}" | xxd -p | tr -d '\n')" | tr ' ' '0')")
+data="${method}${stringtype}${stringlen}${stringval}"
 
-	# Format calldata to call submitObservation(uint128) method with the price.
-	price_u128=$(printf '%064x' ${price})
-	method="dae1ee1f" # Keccak4("submitObservation(uint128)")
-	data="${method}${price_u128}"
-
-	# Submit it to the Sapphire contract.
-	curl -s \
-		--json '{"tx": {"kind": "eth", "data": {"gas_limit": 200000, "to": "'${CONTRACT_ADDRESS}'", "value": 0, "data": "'${data}'"}}}' \
-		--unix-socket /run/rofl-appd.sock \
-  		http://localhost/rofl/v1/tx/sign-submit >/dev/null
-
-  	# Sleep for a while.
-	sleep 60
-done
+# Submit it to the Sapphire contract Echo.sol on testnet
+curl -s \
+	--json '{"tx": {"kind": "eth", "data": {"gas_limit": 200000, "to": "'${CONTRACT_ADDRESS#0x}'", "value": 0, "data": "'${data#0x}'"}}}' \
+	--unix-socket /run/rofl-appd.sock \
+		http://localhost/rofl/v1/tx/sign-submit >/dev/null
